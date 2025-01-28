b5e5c1a0625fb13fb96dad9e1ef68eb4213f4864
Rishuraj Singh <102401151+rraawww10@users.noreply.github.com>
2025-01-28 21:40:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,8 @@ const WeatherDisplay = ({ weather }) => {
 
   return (
     <div>
-      <span style={temperatureStyle}>Temperature: {temperature}&deg;C</span> 
-      <span>Conditions: {conditions}</span>
+      <p style={temperatureStyle}><span>Temperature: {temperature}&deg;C</span></p> 
+      <p><span>Conditions: {conditions}</span></p>
     </div>
   );
 };
